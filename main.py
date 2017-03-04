0fd2d1c2b2003e66f21e0947bce583c0e6cfe6dd
taylorjflatt@gmail.com <taylorjflatt@gmail.com>
2017-03-04 11:47:31 -0600

    Added prga.

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
 import sys
-import base64
-
-user_input_plaintext = input("Please enter your message: ")
-user_input_key = input("Please enter the key: ")
-
-plaintext = base64.b64decode(user_input_plaintext)
 
 def ksa(key):
     """Produces the key for RC4"""
@@ -15,3 +9,25 @@ def ksa(key):
         j = (j + s[i] + key[i % len(key)]) % 256
         s[i], s[j] = s[j], s[i]
     return s
+
+def prga(s):
+    i = j = 0
+    while True:
+        i = (i + 1) % 256
+        j = (j + s[i]) % 256
+        s[i], s[j] = s[j], s[i]
+        yield s[(s[i] + s[j]) % 256]
+
+def main():
+    user_input_plaintext = input("Please enter your message: ").split(' ')
+    user_input_plaintext2 = input("Please enter your message: ")
+    user_input_key = input("Please enter the key: ").split(' ')
+
+    plaintext = [int(num) for num in user_input_plaintext]
+    key = [int(num) for num in user_input_key]
+    
+    print(plaintext)
+    print(key)
+
+if __name__ == "__main__":
+    main()
